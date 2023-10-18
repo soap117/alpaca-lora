b27522d1db831121b909e380f3a760bfdd3ea1ce
asbljy <asbljy@outlook.com>
2023-10-18 17:24:44 -0400

    update

--- finetune.py
+++ finetune.py
@@ -49,10 +49,10 @@ def train(
     add_eos_token: bool = False,
     group_by_length: bool = False,  # faster, but produces an odd training loss curve
     # wandb params
-    wandb_project: str = "",
-    wandb_run_name: str = "",
-    wandb_watch: str = "",  # options: false | gradients | all
-    wandb_log_model: str = "",  # options: false | true
+    wandb_project: str = "LLM-zhihu",
+    wandb_run_name: str = "zhihu-7b",
+    wandb_watch: str = "all",  # options: false | gradients | all
+    wandb_log_model: str = "true",  # options: false | true
     resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
     prompt_template_name: str = "alpaca",  # The prompt template to use, will default to alpaca.
 ):
