ee68a28d97cfe611a78586c6b2b1662061228271
asbljy <asbljy@outlook.com>
2023-10-18 17:32:55 -0400

    update

--- finetune.py
+++ finetune.py
@@ -34,7 +34,7 @@ def train(
     micro_batch_size: int = 8,
     num_epochs: int = 3,
     learning_rate: float = 3e-4,
-    cutoff_len: int = 256,
+    cutoff_len: int = 512,
     val_set_size: int = 2000,
     # lora hyperparams
     lora_r: int = 8,
