6cac302d14d7e3348a53914d0c6b4834b7cf01f6
asbljy <asbljy@outlook.com>
2023-10-18 21:54:05 -0400

    update

--- finetune_ori.py
+++ finetune_ori.py
@@ -5,7 +5,6 @@ from typing import List
 import fire
 import torch
 import transformers
-from datasets import list_datasets, load_from_disk
 from datasets import load_dataset
 
 """
@@ -18,57 +17,29 @@ from peft import (
     LoraConfig,
     get_peft_model,
     get_peft_model_state_dict,
+    prepare_model_for_int8_training,
     set_peft_model_state_dict,
 )
 from transformers import LlamaForCausalLM, LlamaTokenizer
 
 from utils.prompter import Prompter
 
-from transformers import TrainerCallback
 
-class MyCallback(TrainerCallback):
-    def __init__(self, trainer, tokenizer):
-        self.tokenizer = tokenizer
-        self._trainer = trainer
-    def on_step_end(self, args, state, control, **kwargs):
-        if state.global_step % 10 == 0:
-            #generate the predicted output of the model and print it
-            model = self._trainer.model
-            tokenizer = self.tokenizer
-            test_input =\
-                """
-                ### Instruction:
-                你需要模拟一个中文互联网平台知乎上的用户回答以下问题：如何评价美国的社会制度？
-                
-                ### Response:
-                
-                """
-            test_input = tokenizer.encode(test_input, return_tensors="pt")
-            #map to model device
-            test_input = test_input.to(model.device)
-            output = model.generate(
-                num_beams=1,
-                do_sample=False,
-                input_ids=test_input,
-                max_length=512,
-            )
-            print(output.shape)
-            print("Generated output: ", self.tokenizer.decode(output[0], skip_special_tokens=True))
 def train(
     # model/data params
-    base_model: str = "meta-llama/Llama-2-7b-chat-hf",  # the only required argument
-    data_path: str = "social_opinion_zhihu.json",
+	base_model: str = "meta-llama/Llama-2-7b-chat-hf",  # the only required argument
+    data_path: str = "yahma/alpaca-cleaned",
     output_dir: str = "/data/junyu/lora-alpaca",
     # training hyperparams
-    batch_size: int = 64,
-    micro_batch_size: int = 8,
+    batch_size: int = 128,
+    micro_batch_size: int = 4,
     num_epochs: int = 3,
     learning_rate: float = 3e-4,
-    cutoff_len: int = 512,
+    cutoff_len: int = 256,
     val_set_size: int = 2000,
     # lora hyperparams
-    lora_r: int = 16,
-    lora_alpha: int = 64,
+    lora_r: int = 8,
+    lora_alpha: int = 16,
     lora_dropout: float = 0.05,
     lora_target_modules: List[str] = [
         "q_proj",
@@ -79,10 +50,10 @@ def train(
     add_eos_token: bool = False,
     group_by_length: bool = False,  # faster, but produces an odd training loss curve
     # wandb params
-    wandb_project: str = "LLM-zhihu",
-    wandb_run_name: str = "zhihu-7b",
-    wandb_watch: str = "false",  # options: false | gradients | all
-    wandb_log_model: str = "true",  # options: false | true
+    wandb_project: str = "",
+    wandb_run_name: str = "",
+    wandb_watch: str = "",  # options: false | gradients | all
+    wandb_log_model: str = "",  # options: false | true
     resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
     prompt_template_name: str = "alpaca",  # The prompt template to use, will default to alpaca.
 ):
@@ -119,7 +90,7 @@ def train(
 
     prompter = Prompter(prompt_template_name)
 
-    device_map = "cuda"
+    device_map = "auto"
     world_size = int(os.environ.get("WORLD_SIZE", 1))
     ddp = world_size != 1
     if ddp:
@@ -130,7 +101,6 @@ def train(
     use_wandb = len(wandb_project) > 0 or (
         "WANDB_PROJECT" in os.environ and len(os.environ["WANDB_PROJECT"]) > 0
     )
-    use_wandb = False
     # Only overwrite environ if wandb param passed
     if len(wandb_project) > 0:
         os.environ["WANDB_PROJECT"] = wandb_project
@@ -141,11 +111,11 @@ def train(
 
     model = LlamaForCausalLM.from_pretrained(
         base_model,
+        load_in_8bit=True,
         torch_dtype=torch.float16,
         device_map=device_map,
-        cache_dir="./cache/",
+	    cache_dir="./cache/",
     )
-    model.half()
 
     tokenizer = LlamaTokenizer.from_pretrained(base_model)
 
@@ -202,6 +172,7 @@ def train(
             ]  # could be sped up, probably
         return tokenized_full_prompt
 
+    model = prepare_model_for_int8_training(model)
 
     config = LoraConfig(
         r=lora_r,
@@ -240,33 +211,25 @@ def train(
 
     model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
 
-    #load the data if exists
-
-    if os.path.exists("train_data.data") and os.path.exists("val_data.data"):
-        train_data = load_from_disk("train_data.data")
-        val_data = load_from_disk("val_data.data")
+    if val_set_size > 0:
+        train_val = data["train"].train_test_split(
+            test_size=val_set_size, shuffle=True, seed=42
+        )
+        train_data = (
+            train_val["train"].shuffle().map(generate_and_tokenize_prompt)
+        )
+        val_data = (
+            train_val["test"].shuffle().map(generate_and_tokenize_prompt)
+        )
     else:
-        if val_set_size > 0:
-            train_val = data["train"].train_test_split(
-                test_size=val_set_size, shuffle=True, seed=42
-            )
-            train_data = (
-                train_val["train"].shuffle().map(generate_and_tokenize_prompt)
-            )
-            val_data = (
-                train_val["test"].shuffle().map(generate_and_tokenize_prompt)
-            )
-        else:
-            train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
-            val_data = None
-        # save the datasets
-        train_data.save_to_disk("train_data.data")
-        val_data.save_to_disk("val_data.data")
+        train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
+        val_data = None
 
     if not ddp and torch.cuda.device_count() > 1:
         # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
         model.is_parallelizable = True
         model.model_parallel = True
+
     trainer = transformers.Trainer(
         model=model,
         train_dataset=train_data,
@@ -274,6 +237,7 @@ def train(
         args=transformers.TrainingArguments(
             per_device_train_batch_size=micro_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
+            warmup_steps=100,
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
             fp16=True,
@@ -295,7 +259,6 @@ def train(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
         ),
     )
-    trainer.add_callback(MyCallback(trainer, tokenizer))
     model.config.use_cache = False
 
     old_state_dict = model.state_dict
