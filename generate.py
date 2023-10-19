1a6061d8a59fc22bf6577cc827b63306aecc7fa6
asbljy <asbljy@outlook.com>
2023-10-19 16:15:58 -0400

    update

--- generate.py
+++ generate.py
@@ -45,11 +45,11 @@ def main(
             torch_dtype=torch.bfloat16,
             device_map="auto",
         )
-        model = PeftModel.from_pretrained(
-            model,
-            lora_weights,
-            torch_dtype=torch.float16,
-        )
+        #model = PeftModel.from_pretrained(
+        #    model,
+        #    lora_weights,
+        #    torch_dtype=torch.float16,
+        #)
     elif device == "mps":
         model = LlamaForCausalLM.from_pretrained(
             base_model,
