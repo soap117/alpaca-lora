029de27fb424d827d4ddf68d3f56971ed2acf2ae
asbljy <asbljy@outlook.com>
2023-10-18 21:55:36 -0400

    update

--- finetune_ori.py
+++ finetune_ori.py
@@ -50,10 +50,10 @@ def train(
     add_eos_token: bool = False,
     group_by_length: bool = False,  # faster, but produces an odd training loss curve
     # wandb params
-    wandb_project: str = "",
-    wandb_run_name: str = "",
-    wandb_watch: str = "",  # options: false | gradients | all
-    wandb_log_model: str = "",  # options: false | true
+    wandb_project: str = "LLM-ori",
+    wandb_run_name: str = "LLAMA2-7b",
+    wandb_watch: str = "false",  # options: false | gradients | all
+    wandb_log_model: str = "true",  # options: false | true
     resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
     prompt_template_name: str = "alpaca",  # The prompt template to use, will default to alpaca.
 ):
