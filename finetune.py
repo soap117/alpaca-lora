7e679697ad06fbb114ff49c9a06302bb5fadb67f
asbljy <asbljy@outlook.com>
2023-10-19 17:10:40 -0400

    update

--- finetune.py
+++ finetune.py
@@ -327,7 +327,7 @@ def train(
 
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
-
+    print("checkpoint", resume_from_checkpoint)
     trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
     model.save_pretrained(output_dir)
